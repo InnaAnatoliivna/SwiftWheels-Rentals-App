db4270c061badd454c1f89ce7bc10ac95ec9afe9
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-11-13 23:37:34 +0100

    + styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
 import Container from '../Container/Container'
 import { LinkLogo, LinkStyled, Section, Wrapper } from './Header.styled'
-// import { Link } from 'react-router-dom'
+import logo from '../../icon/car-vehicle-line.png'
+
 
 const Header = () => {
     return (
         <Section>
             <Container>
                 <Wrapper>
-                    <p><LinkLogo to='/'>SwiftWheels Rentals</LinkLogo></p>
+                    <p><LinkLogo to='/'>
+                        <img src={logo} width='150' />
+                        SwiftWheels Rentals</LinkLogo></p>
                     <nav>
                         <LinkStyled to='/catalog'>Catalog</LinkStyled>
                         <LinkStyled to='/favorites'>Favorites</LinkStyled>
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -2,17 +2,17 @@ import { Link, NavLink } from "react-router-dom"
 import styled from "styled-components"
 
 export const Section = styled.div`
-    height: 78px;
+    height: 74px;
     background: #004FF9;  /* fallback for old browsers */
     background: -webkit-linear-gradient(to right, #FFF94C, #004FF9);  /* Chrome 10-25, Safari 5.1-6 */
     background: linear-gradient(to right, #FFF94C, #004FF9); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-    margin-bottom: 40px;
+    /* margin-bottom: 40px; */
 `;
 
 export const LinkStyled = styled(NavLink)`
-    padding: 24.5px 8px;
-    color: #fffc00;
-    font-size: 22px;
+    padding: 24px 8px;
+    color: var(--color-hero);
+    font-size: 20px;
     font-weight: 500;
     line-height: 1.2;
     transition: color linear 500ms;
@@ -20,16 +20,19 @@ export const LinkStyled = styled(NavLink)`
         color: #fff;
     }
     &.active {
-        border-bottom: 3px solid #fffc00;
+        border-bottom: 3px solid var(--color-logo);
     }
 `;
 
 export const LinkLogo = styled(Link)`
-        text-decoration: none;
-        color: var(--color-accent);
-        font-size: 32px;
-        font-weight: 500;
-        line-height: 1;`;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    color: var(--color-logo);
+    font-size: 24px;
+    font-weight: 500;
+    line-height: 1;`;
 
 export const Wrapper = styled.div`
     display: flex;
