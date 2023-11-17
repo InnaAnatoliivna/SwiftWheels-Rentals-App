7e4f77e8746257fa57cd9443cf4f40bdcf74bc63
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-11-17 23:37:10 +0100

    +

--- src/pages/CatalogPage/CatalogPage.styled.jsx
+++ src/pages/CatalogPage/CatalogPage.styled.jsx
@@ -1,6 +1,6 @@
-import styled from 'styled-components'
-import LoadMoreButton from '../../components/LoadMore/LoadMore'
+// import styled from 'styled-components'
+// import LoadMoreButton from '../../components/LoadMore/LoadMore'
 
-export const LoadMoreStyled = styled(LoadMoreButton)`
-    margin-bottom: 70px;
-`
+// export const LoadMoreStyled = styled(LoadMoreButton)`
+//     margin-bottom: 70px;
+// `
