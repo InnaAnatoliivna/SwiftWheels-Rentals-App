9d491fda72bbcbcae45ba7d534ba84e04e0bdb9d
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-11-17 22:37:58 +0000

    Deploying to gh-pages from @ InnaAnatoliivna/SwiftWheels-Rentals-App@fdff21b75ed158e3049caa6fa3a204859f995f6c 🚀

--- assets/ErrorPage-680d9fe0.js
+++ assets/ErrorPage-680d9fe0.js
@@ -1,4 +1,4 @@
-import{s as r,u as i,r as a,j as e}from"./index-025d53e1.js";const n=r.div`
+import{s as r,u as i,r as a,j as e}from"./index-abb62d32.js";const n=r.div`
   padding: 200px 0;
 `,o=r.div`
   color: var(--color-text);
