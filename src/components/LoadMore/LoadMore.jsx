74efefde98601e60d5c9a7076feb90af050f320c
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-11-18 20:07:51 +0100

    Update LoadMore.jsx

--- src/components/LoadMore/LoadMore.jsx
+++ src/components/LoadMore/LoadMore.jsx
@@ -3,10 +3,16 @@ import { Wrapper } from './LoadMore.styled';
 
 const LoadMoreButton = ({ onLoadMore }) => {
 
+    console.log('adverts LOAD MORE : START ')
+
+
     const handleLoadMore = async (event) => {
         event.preventDefault();
         onLoadMore();
     };
+
+    console.log('adverts LOAD MORE : END ')
+
 
     return (
         <Wrapper>
