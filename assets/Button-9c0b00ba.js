a0c3bcfd7e8b24a20ba8708dc2f979a282d091b8
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-11-18 21:47:46 +0000

    Deploying to gh-pages from @ InnaAnatoliivna/SwiftWheels-Rentals-App@7b230ce16d6a08f946e8ed7692a3b047f7723c4e 🚀

--- assets/Button-9c0b00ba.js
+++ assets/Button-9c0b00ba.js
@@ -1,4 +1,4 @@
-import{s as n,j as t}from"./index-3bb3bce1.js";const e=n.button`
+import{s as n,j as t}from"./index-f425cfc4.js";const e=n.button`
     display: flex;
     width: 274px;
     height: 44px;
---
+++ assets/FavoritesPage-50644ec1.js
@@ -0,0 +1,19 @@
+import{s as g,u as d,a as p,r as a,j as e,C as h,b as v}from"./index-f425cfc4.js";import{c as x,A as f}from"./AdvertsList-0394c53b.js";import{B as u}from"./Button-9c0b00ba.js";const m=g.div`
+    height: 100vh;
+
+    /* padding: 200px 0; */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 28px;
+     p {
+        color: var(--color-text);
+        width: 100%;
+        font-size: 40px;
+        font-weight: 700;
+        line-height: 1.2; /* 111.429% */
+        letter-spacing: 0.7px;
+        text-align: center;
+    }
+`,w=()=>{const o=d(),s=p(x),[r,i]=a.useState([]),n=12;return a.useEffect(()=>{(async()=>{try{const t=await v();if(t){const c=t.filter(l=>s.includes(l.id));i(c.slice(0,currentPage*n))}}catch(t){throw console.error("Error fetching adverts: ",t.message),t}})()},[s,currentPage]),e.jsx(h,{children:r.length>0?e.jsx(f,{adverts:r}):e.jsxs(m,{children:[e.jsx("p",{children:"Unfortunately, the list is empty."}),e.jsx(u,{onClick:()=>o("/catalog"),children:"Back to the catalog"})]})})};export{w as default};
