49acf8f6ccb1c25730c0a6b745e2f3c136e9d28a
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-11-18 22:44:33 +0100

    Update LoadMore.jsx

--- src/components/LoadMore/LoadMore.jsx
+++ src/components/LoadMore/LoadMore.jsx
@@ -1,25 +1,25 @@
-import Button from '../Button/Button';
-import { Wrapper } from './LoadMore.styled';
+// import Button from '../Button/Button';
+// import { Wrapper } from './LoadMore.styled';
 
-const LoadMoreButton = ({ onLoadMore }) => {
+// const LoadMoreButton = ({ onLoadMore }) => {
 
-    console.log('adverts LOAD MORE : START ')
-
-
-    const handleLoadMore = async (event) => {
-        event.preventDefault();
-        onLoadMore();
-    };
-
-    console.log('adverts LOAD MORE : END ')
+//     console.log('adverts LOAD MORE : START ')
 
 
-    return (
-        <Wrapper>
-            <Button onClick={handleLoadMore}>Load more</Button>
-        </Wrapper>
-    );
-}
+//     const handleLoadMore = async (event) => {
+//         event.preventDefault();
+//         onLoadMore();
+//     };
+
+//     console.log('adverts LOAD MORE : END ')
 
 
-export default LoadMoreButton;
+//     return (
+//         <Wrapper>
+//             <Button onClick={handleLoadMore}>Load more</Button>
+//         </Wrapper>
+//     );
+// }
+
+
+// export default LoadMoreButton;
